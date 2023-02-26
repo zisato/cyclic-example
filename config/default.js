365cf76336f04cd870066f8e917c47fb21335f68
Javier Rodriguez <javierrodriguezcuevas@gmail.com>
2023-02-26 23:03:57 +0100

    refactor file type

---
+++ config/default.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  container: {
+    loadModules: [
+      'src/infrastructure/**/*.ts'
+    ]
+  },
+  expressServer: {
+    port: process.env.HTTP_PORT,
+    middlewares: [
+      'express.cors',
+      'express.json',
+      {
+        'express.static': {
+          dir: path.join(__dirname, '..', 'public')
+        }
+      }
+    ]
+  }
+}
