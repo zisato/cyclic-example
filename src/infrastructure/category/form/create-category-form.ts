3f8667e14ac5011bdf6bac7ad9a63c745c07135e
Javier Rodriguez <javierrodriguezcuevas@gmail.com>
2023-05-01 12:28:59 +0200

    create category form

---
+++ src/infrastructure/category/form/create-category-form.ts
@@ -0,0 +1,47 @@
+import * as joi from 'joi'
+import { Request } from 'express'
+import { ValidationError } from 'joi'
+
+type CreateCategoryFormData = {
+    attributes: {
+        name: string
+    }
+}
+
+export class CreateCategoryForm {
+    private validationError?: ValidationError = undefined
+    private data: CreateCategoryFormData | null = null
+
+    async handleRequest(request: Request): Promise<void> {
+        const validationResult = this.getValidationBodyResult(request.body)
+        if (validationResult.error !== undefined) {
+            this.validationError = validationResult.error
+
+            return
+        }
+
+        this.data = validationResult.value
+    }
+
+    isValid(): boolean {
+        return this.validationError === undefined
+    }
+
+    getData(): CreateCategoryFormData {
+        if (this.data === null) {
+            throw new Error('Form not handled')
+        }
+
+        return this.data
+    }
+
+    private getValidationBodyResult(data: any): joi.ValidationResult<CreateCategoryFormData> {
+        const schema = joi.object({
+            attributes: joi.object({
+                name: joi.string().required()
+            }).required(),
+        })
+
+        return schema.validate(data)
+    }
+}
