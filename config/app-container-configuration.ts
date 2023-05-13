28fd67e8a29744f6cb04653ced3a3229ff480a72
Javier Rodriguez <javierrodriguezcuevas@gmail.com>
2023-05-13 02:43:03 +0200

    use in memory storage

--- config/app-container-configuration.ts
+++ config/app-container-configuration.ts
@@ -7,8 +7,8 @@ import AppErrorHandlerMiddleware from '../src/infrastructure/express/middleware/
 import { InvalidArgumentError } from '../src/domain/error/invalid-argument-error'
 import { ModelNotFoundError } from '../src/domain/error/model-not-found-error'
 import { InvalidJsonSchemaError } from '../src/infrastructure/error/invalid-json-schema-error'
-import { S3FileStorageService } from '../src/infrastructure/file-storage/s3-file-storage-service'
-import { S3 } from '@aws-sdk/client-s3'
+// import { S3FileStorageService } from '../src/infrastructure/file-storage/s3-file-storage-service'
+// import { S3 } from '@aws-sdk/client-s3'
 
 export class AppContainerConfiguration implements ContainerConfiguration {
     configureContainer(container: AwilixContainer, parameters: Parameters): void {
@@ -23,16 +23,16 @@ export class AppContainerConfiguration implements ContainerConfiguration {
 
         container.register({
             // cyclicDB: asValue(CyclicDB('long-lime-whale-garbCyclicDB')),
-            s3Client: asClass(S3).inject(() => ({
-                configuration: {
-                    endpoint: parameters.get<string>('s3.endpoint'),
-                    forcePathStyle: parameters.get<boolean>('s3.forcePathStyle')
-                }
-            })),
-            fileStorageService: asClass(S3FileStorageService).inject(() => ({
-                bucketName: parameters.get<string>('s3.bucketName')
-            })),
-            // fileStorageService: aliasTo('inMemoryFileStorageService'),
+            // s3Client: asClass(S3).inject(() => ({
+            //     configuration: {
+            //         endpoint: parameters.get<string>('s3.endpoint'),
+            //         forcePathStyle: parameters.get<boolean>('s3.forcePathStyle')
+            //     }
+            // })),
+            // fileStorageService: asClass(S3FileStorageService).inject(() => ({
+            //     bucketName: parameters.get<string>('s3.bucketName')
+            // })),
+            fileStorageService: aliasTo('inMemoryFileStorageService'),
             adminListProductsController: asClass(AdminListProductsController),
             listProductsController: asClass(ListProductsController),
             productRepository: aliasTo('inMemoryProductRepository'),
